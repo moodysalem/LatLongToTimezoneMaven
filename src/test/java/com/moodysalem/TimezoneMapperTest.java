226ba770849f20eea4a517159063aa640e346f2e
Moody Salem <moody@fastmodeltechnologies.com>
2017-02-23 16:43:51 -0600

    clean up and speed up tests

--- src/test/java/com/moodysalem/TimezoneMapperTest.java
+++ src/test/java/com/moodysalem/TimezoneMapperTest.java
@@ -3,87 +3,72 @@ package com.moodysalem;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
-import org.testng.annotations.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import static org.testng.AssertJUnit.*;
+import static org.testng.AssertJUnit.assertTrue;
 
 public class TimezoneMapperTest {
-
     private static final String EXPECT_ERROR = "City: %s, Lat: %s, Lng: %s, Expected: %s, Got: %s";
     private static final Logger LOG = Logger.getLogger(TimezoneMapperTest.class.getName());
 
-    public static class TestData {
-        private String name;
-        private double lat;
-        private double lng;
-        private String expectedTimezone;
+    private static class Test {
+        private String name, expectedTimezone;
+        private final double lat, lng;
 
-        public TestData(String name, double lat, double lng, String expectedTimezone) {
-            if (name == null || expectedTimezone == null) {
-                throw new NullPointerException();
-            }
+        private Test(final String name, final double lat, final double lng, final String expectedTimezone) {
             this.name = name;
             this.lat = lat;
             this.lng = lng;
             this.expectedTimezone = expectedTimezone;
         }
-
-        public String getName() {
-            return name;
-        }
-
-        public double getLat() {
-            return lat;
-        }
-
-        public double getLng() {
-            return lng;
-        }
-
-        public String getExpectedTimezone() {
-            return expectedTimezone;
-        }
     }
 
-    private static final List<TestData> testData = new ArrayList<>();
+    private static final List<Test> tests = new LinkedList<>();
 
     static {
-        try (InputStream is = TimezoneMapperTest.class.getClassLoader().getResourceAsStream("test-cases.csv");
-             CSVParser cp = CSVFormat.DEFAULT.withHeader().parse(new InputStreamReader(is))) {
-            for (CSVRecord cr : cp) {
-                testData.add(new TestData(cr.get("name"),
-                        Double.parseDouble(cr.get("lat")), Double.parseDouble(cr.get("lng")),
-                        cr.get("zone")));
+        try (final InputStream is = TimezoneMapperTest.class.getClassLoader().getResourceAsStream("test-cases.csv");
+             final CSVParser cp = CSVFormat.DEFAULT.withHeader().parse(new InputStreamReader(is))) {
+            for (final CSVRecord cr : cp) {
+                tests.add(
+                        new Test(
+                                cr.get("name"),
+                                Double.parseDouble(cr.get("lat")),
+                                Double.parseDouble(cr.get("lng")),
+                                cr.get("zone")
+                        )
+                );
             }
         } catch (IOException e) {
             LOG.log(Level.SEVERE, "Failed to parse test file", e);
         }
     }
 
-    @Test
-    public void runTestCases() {
-        final AtomicInteger succeeded = new AtomicInteger(0), failed = new AtomicInteger(0);
+    @org.testng.annotations.Test
+    public void testTzNameAt() {
+        final AtomicInteger succeeded = new AtomicInteger(0),
+                failed = new AtomicInteger(0);
 
-        for (TestData data: testData) {
-            String timezone = TimezoneMapper.tzNameAt(data.getLat(), data.getLng());
+        for (final Test test : tests) {
+            final String timezone = TimezoneMapper.tzNameAt(test.lat, test.lng);
 
-            String error = String.format(EXPECT_ERROR, data.getName(), data.getLat(), data.getLng(),
-                    data.getExpectedTimezone(), timezone);
-            try {
-                assertTrue(error, data.getExpectedTimezone().equals(timezone));
+            if (test.expectedTimezone.equals(timezone)) {
                 succeeded.incrementAndGet();
-            } catch (AssertionError e) {
+            } else {
                 failed.incrementAndGet();
-                LOG.warning(e.getMessage());
+                LOG.warning(
+                        String.format(
+                                EXPECT_ERROR,
+                                test.name, test.lat, test.lng, test.expectedTimezone, timezone
+                        )
+                );
             }
         }
 
